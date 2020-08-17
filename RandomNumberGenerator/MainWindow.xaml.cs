47acf677f5d96b8c8915693a4ed39b92d2f8dfd0
Balttazarr - CzarnyPotwor PC <felekmakuta@wp.pl>
2020-08-17 21:12:12 +0200

    Add button logic to generate random numbers

--- RandomNumberGenerator/MainWindow.xaml.cs
+++ RandomNumberGenerator/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+
 
 namespace RandomNumberGenerator
 {
@@ -27,7 +30,43 @@ namespace RandomNumberGenerator
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            listBox1.Items.Clear();
+            bool op1ok = int.TryParse(txt_RMin.Text, out int min);
 
+            bool op2ok = int.TryParse(txt_RMax.Text, out int max);
+
+            if (op1ok == false || op2ok == false)
+                throw new ArgumentException("wrong numbers");
+
+            int hownumbers = (int)slValue.Value;
+
+            string url = @"http://www.random.org/integers/?num=" +
+                hownumbers.ToString() +
+                "&min=" + min.ToString() +
+                "&max=" + max.ToString() +
+                "&col=1&base=10&format=plain&rnd=new";
+            try
+            {
+                string data = HtmlDownload.HtmlToString(url);
+
+                string[] dataArray = data.Split('\n');
+
+                foreach (var item in dataArray)
+                {
+                    listBox1.Items.Add(item);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
